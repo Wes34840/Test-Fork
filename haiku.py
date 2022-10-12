54a6db067f600fd21413f255c7c4bd7f31b42454
Wessel <34840@ma-web.nl>
2022-10-12 14:44:03 +0200

    haiku

---
+++ haiku.py
@@ -0,0 +1,22 @@
+
+
+#plak hier de haiku functions onder elkaar
+def Haiku1():
+    print("haiku by:","Kobayahsi Issa")
+    print("The wren Earns his living Noiselessly ") 
+
+def Haiku2():
+    print("haiku by:","Matsuo Bashō") 
+    print("From time to time") 
+    print("The clouds give rest To the moon-beholders.") 
+
+def Haiku3():
+    print("haiku by:","Yosa Buson") 
+    print("Over-ripe sushi")
+    print("The Master Is full of regret.")
+
+
+#laat de code hieronder met rust
+Haiku1()
+Haiku2()
+Haiku3()
